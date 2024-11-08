a9d5d5b999c9ce2bc8cd4bf9a3f1cd1d6a9238a9
Lê Huy Hoàng <156869304+hoangscor@users.noreply.github.com>
2024-11-08 19:08:06 +0700

    them idenDBcontext

---
+++ HairmonySalon.Reponsitories/Entities/IdentityCoreDbContext.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Harmony.Repositories.Entities;
+
+public partial class IdentityCoreDbContext : DbContext
+{
+    public IdentityCoreDbContext()
+    {
+    }
+
+    public IdentityCoreDbContext(DbContextOptions<IdentityCoreDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<AspNetRole> AspNetRoles { get; set; }
+
+    public virtual DbSet<AspNetRoleClaim> AspNetRoleClaims { get; set; }
+
+    public virtual DbSet<AspNetUser> AspNetUsers { get; set; }
+
+    public virtual DbSet<AspNetUserClaim> AspNetUserClaims { get; set; }
+
+    public virtual DbSet<AspNetUserLogin> AspNetUserLogins { get; set; }
+
+    public virtual DbSet<AspNetUserToken> AspNetUserTokens { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=HOANGDZ\\SQLSV;Initial Catalog=IdentityCoreDB;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<AspNetRole>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__AspNetRo__3214EC07E0C440E2");
+
+            entity.HasIndex(e => e.Name, "UQ__AspNetRo__737584F6C84C04F6").IsUnique();
+
+            entity.HasIndex(e => e.NormalizedName, "UQ__AspNetRo__A93C97B9A4E8E767").IsUnique();
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.Name).HasMaxLength(256);
+            entity.Property(e => e.NormalizedName).HasMaxLength(256);
+        });
+
+        modelBuilder.Entity<AspNetRoleClaim>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__AspNetRo__3214EC073EA33A8C");
+
+            entity.Property(e => e.ClaimType).HasMaxLength(256);
+            entity.Property(e => e.ClaimValue).HasMaxLength(256);
+
+            entity.HasOne(d => d.Role).WithMany(p => p.AspNetRoleClaims)
+                .HasForeignKey(d => d.RoleId)
+                .HasConstraintName("FK__AspNetRol__RoleI__5070F446");
+        });
+
+        modelBuilder.Entity<AspNetUser>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__AspNetUs__3214EC07C745D391");
+
+            entity.HasIndex(e => e.NormalizedUserName, "UQ__AspNetUs__54E8BE2211128E5D").IsUnique();
+
+            entity.HasIndex(e => e.UserName, "UQ__AspNetUs__C9F284565B28F2B9").IsUnique();
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.Email).HasMaxLength(256);
+            entity.Property(e => e.NormalizedEmail).HasMaxLength(256);
+            entity.Property(e => e.NormalizedUserName).HasMaxLength(256);
+            entity.Property(e => e.PhoneNumber).HasMaxLength(15);
+            entity.Property(e => e.UserName).HasMaxLength(256);
+
+            entity.HasMany(d => d.Roles).WithMany(p => p.Users)
+                .UsingEntity<Dictionary<string, object>>(
+                    "AspNetUserRole",
+                    r => r.HasOne<AspNetRole>().WithMany()
+                        .HasForeignKey("RoleId")
+                        .HasConstraintName("FK__AspNetUse__RoleI__44FF419A"),
+                    l => l.HasOne<AspNetUser>().WithMany()
+                        .HasForeignKey("UserId")
+                        .HasConstraintName("FK__AspNetUse__UserI__440B1D61"),
+                    j =>
+                    {
+                        j.HasKey("UserId", "RoleId").HasName("PK__AspNetUs__AF2760AD183399C5");
+                        j.ToTable("AspNetUserRoles");
+                    });
+        });
+
+        modelBuilder.Entity<AspNetUserClaim>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__AspNetUs__3214EC0774C48A12");
+
+            entity.Property(e => e.ClaimType).HasMaxLength(256);
+            entity.Property(e => e.ClaimValue).HasMaxLength(256);
+
+            entity.HasOne(d => d.User).WithMany(p => p.AspNetUserClaims)
+                .HasForeignKey(d => d.UserId)
+                .HasConstraintName("FK__AspNetUse__UserI__47DBAE45");
+        });
+
+        modelBuilder.Entity<AspNetUserLogin>(entity =>
+        {
+            entity.HasKey(e => new { e.LoginProvider, e.ProviderKey }).HasName("PK__AspNetUs__2B2C5B521ADB92B9");
+
+            entity.Property(e => e.LoginProvider).HasMaxLength(128);
+            entity.Property(e => e.ProviderKey).HasMaxLength(256);
+            entity.Property(e => e.ProviderDisplayName).HasMaxLength(256);
+
+            entity.HasOne(d => d.User).WithMany(p => p.AspNetUserLogins)
+                .HasForeignKey(d => d.UserId)
+                .HasConstraintName("FK__AspNetUse__UserI__4AB81AF0");
+        });
+
+        modelBuilder.Entity<AspNetUserToken>(entity =>
+        {
+            entity.HasKey(e => new { e.UserId, e.LoginProvider, e.Name }).HasName("PK__AspNetUs__8CC498413755731B");
+
+            entity.Property(e => e.LoginProvider).HasMaxLength(128);
+            entity.Property(e => e.Name).HasMaxLength(128);
+
+            entity.HasOne(d => d.User).WithMany(p => p.AspNetUserTokens)
+                .HasForeignKey(d => d.UserId)
+                .HasConstraintName("FK__AspNetUse__UserI__4D94879B");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
