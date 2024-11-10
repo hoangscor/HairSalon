6e49b2006cc0b781510b19fb25e0d48eec132f35
Lê Huy Hoàng <156869304+hoangscor@users.noreply.github.com>
2024-11-10 14:57:13 +0700

    sua UR cho phu hop với db mới

--- HairmonySalon.Reponsitories/UserRepository.cs
+++ HairmonySalon.Reponsitories/UserRepository.cs
@@ -9,17 +9,17 @@ using HarmonySalon.Reponsitories.Entities;
 
 namespace Harmony.Repositories
 {
-	public class UserRepository : IUserRepository
-	{
-		private readonly HarmonySalonContext _dbContext;
-		public UserRepository(HarmonySalonContext dbContext)
-		{
-			_dbContext = dbContext;
-		}
+    public class UserRepository : IUserRepository
+    {
+        private readonly HarmonySalonContext _dbContext;
+        public UserRepository(HarmonySalonContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
 
-		public async Task<List<User>> GetAllUser()
-		{
-			return await _dbContext.Users.ToListAsync();
-		}
-	}
+        public async Task<List<User>> GetAllUser()
+        {
+            return await _dbContext.Users.ToListAsync();
+        }
+    }
 }
