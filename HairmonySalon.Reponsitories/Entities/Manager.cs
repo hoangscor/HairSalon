5445b6be046e99e7390dbb6c4171640ff542f3f1
Lê Huy Hoàng <156869304+hoangscor@users.noreply.github.com>
2024-11-10 14:59:07 +0700

    add Manager.cs

---
+++ HairmonySalon.Reponsitories/Entities/Manager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HarmonySalon.Reponsitories.Entities;
+
+public partial class Manager
+{
+    public int ManagerId { get; set; }
+
+    public string? Permissions { get; set; }
+
+    public virtual User ManagerNavigation { get; set; } = null!;
+}
