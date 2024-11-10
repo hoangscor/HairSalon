dc9b1c89c641dd25f88ec197a6029af1b6371ff1
Chau Ngoc <chaungoc7909@gmail.com>
2024-11-10 13:25:13 +0700

    add service

---
+++ Harmony.WebApplication/Controllers/ServiceController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Harmony.WebApplication.Controllers
+{
+	public class ServiceController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+		public IActionResult Appointment()
+		{
+			return View();
+		}
+		public IActionResult PickTime()
+		{
+			return View();
+		}
+
+		public IActionResult SelectStylist()
+		{
+			return View();
+		}
+
+	}
+}
