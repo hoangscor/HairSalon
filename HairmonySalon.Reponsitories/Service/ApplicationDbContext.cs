a7e261381813a614b590f62ecf635a05d337ae63
Chau Ngoc <chaungoc7909@gmail.com>
2024-11-19 11:43:50 +0700

    them lk voi database

---
+++ HairmonySalon.Reponsitories/Service/ApplicationDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harmony.Repositories.Service
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+         : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            var admin = new IdentityRole("admin");
+            admin.NormalizedName= "admin";
+
+            var customer = new IdentityRole("customer");
+            customer.NormalizedName = "customer";
+
+            var staff = new IdentityRole("staff");
+            staff.NormalizedName = "staff";
+
+            builder.Entity<IdentityRole>().HasData(admin,customer, staff);
+        }
+
+    }
+}
---
+++ HairmonySalon.Reponsitories/Service/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harmony.Repositories.Service
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FName { get; set; } = "";
+        public string LName { get; set; } = "";
+        public string Address { get; set; } = "";
+        public DateTime CreateAt { get; set; }
+    }
+}
