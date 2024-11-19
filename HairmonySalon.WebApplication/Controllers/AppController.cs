4ab9cf2b0591c7d12c2b22e0ad4a7be8664c0b7f
Lê Huy Hoàng <156869304+hoangscor@users.noreply.github.com>
2024-11-19 13:14:36 +0700

    thêm app Controllers/AppController.cs để đứng từ app gọi các chức năng khác

---
+++ HairmonySalon.WebApplication/Controllers/AppController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace HairHarmonySalon.Controllers
+{
+	public class AppController : Controller
+	{
+		/*public AppController(IHttpContextAccessor httpContextAccessor)
+		{
+			// Kiểm tra session
+			*//*var user_session = HttpContext.Session.GetString("UserName");*//*
+		}*/
+	}
+}
