4cdae978999d35dbee428fc9d8df90c85cfd888c
Lê Huy Hoàng <156869304+hoangscor@users.noreply.github.com>
2024-11-20 13:32:35 +0700

    Thêm Index , ListStylist ,Appointment action

--- HairmonySalon.WebApplication/Controllers/ServicesController.cs
+++ HairmonySalon.WebApplication/Controllers/ServicesController.cs
@@ -2,12 +2,15 @@
 using System.Diagnostics;
 using HairHarmonySalon.Models;
 using HairHarmonySalon.Models.Authentication;
+using Harmony.Repositories.Entities;
+using HairHarmonySalon.ViewModel;
 
 namespace HairHarmonySalon.Controllers
 {
 	public class ServicesController : AppController
 	{
-		public IActionResult Index()
+        HarmonySalonContext db = new HarmonySalonContext();
+        public IActionResult Index()
 		{
 			return View();
 		}
@@ -19,10 +22,31 @@ namespace HairHarmonySalon.Controllers
 		[Authentication]
 		public IActionResult PickTime()
 		{
-			return View();
+            var listStylist = db.Users.Where(u => u.UserType == "Stylist").ToList();
+            var listService = db.Services.ToList();
+            ViewBag.listStylist = listStylist;
+            ViewBag.listService = listService;
+            return View();
 		}
+        [Authentication]
+        [HttpPost]
+        public async Task<IActionResult> PickTime(Appointment model)
+        {
+            var user_id = HttpContext.Session.GetString("UserId");
+            var appointment = new Appointment
+            {
+                CustomerId = int.Parse(user_id),
+                StylistId = model.StylistId,
+                ServiceId = model.ServiceId,
+                AppointmentDate = model.AppointmentDate,
+            };
 
-		public IActionResult SelectStylist()
+            db.Appointments.Add(appointment);
+            db.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult SelectStylist()
 		{
 			return View();
 		}
