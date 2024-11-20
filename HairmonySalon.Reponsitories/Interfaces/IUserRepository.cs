b20c8707d26d8e6059083e2373fdb572d700bda6
Lê Huy Hoàng <156869304+hoangscor@users.noreply.github.com>
2024-11-20 13:41:39 +0700

    Harmony.

--- HairmonySalon.Reponsitories/Interfaces/IUserRepository.cs
+++ HairmonySalon.Reponsitories/Interfaces/IUserRepository.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using HarmonySalon.Reponsitories.Entities;
+using Harmony.Repositories.Entities;
 
 namespace Harmony.Repositories.Interfaces
 {
--- HairmonySalon.Reponsitories/UserRepository.cs
+++ HairmonySalon.Reponsitories/UserRepository.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Harmony.Repositories.Interfaces;
-using HarmonySalon.Reponsitories.Entities;
+using Harmony.Repositories.Entities;
 
 namespace Harmony.Repositories
 {
