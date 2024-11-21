78785b0213dffebe60be03c89c904d7782d1b273
Lê Huy Hoàng <156869304+hoangscor@users.noreply.github.com>
2024-11-21 13:11:15 +0700

    Them SuaAppointment

--- HairmonySalon.WebApplication/Areas/Admin/Controllers/HomeAdminController.cs
+++ HairmonySalon.WebApplication/Areas/Admin/Controllers/HomeAdminController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System;
+using System.Globalization;
 
 namespace HairHarmonySalon.Areas.Admin.Controllers
 {
@@ -19,7 +20,7 @@ namespace HairHarmonySalon.Areas.Admin.Controllers
         public override void OnActionExecuting(ActionExecutingContext context)
         {
             base.OnActionExecuting(context);
-
+            // phân quyền 
             var role = HttpContext.Session.GetString("Role");
             if (role != "Admin" && role != "Stylist" && role != "Manager")
             {
@@ -60,7 +61,7 @@ namespace HairHarmonySalon.Areas.Admin.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult ThemUserMoi(RegisterVM Users)
         {
-
+            // xem trùng email
             var existingUser = db.Users.FirstOrDefault(u => u.Email == Users.Email);
             if (existingUser != null)
             {
@@ -79,6 +80,58 @@ namespace HairHarmonySalon.Areas.Admin.Controllers
             db.SaveChanges();
             return RedirectToAction("DanhMucUser", "admin");
             return View(Users);
+        }
+        //sua User
+        [Route("SuaUser")]
+        [HttpGet]
+        public IActionResult SuaUser(int id)
+        {
+            // Tìm người dùng theo ID
+            var user = db.Users.FirstOrDefault(u => u.UserId == id);
+
+            if (user == null)
+            {
+                // Nếu không tìm thấy người dùng, trả về lỗi
+                ModelState.AddModelError("", "User not found.");
+                return RedirectToAction("DanhMucUser", "admin");
+            }
+            ViewBag.UserId = user.UserId;
+
+            return View();
+        }
+        [Route("SuaUser")]
+        [HttpPost]
+        public IActionResult SuaUser(User suaUser)
+        {
+            // Tìm người dùng theo ID
+            var user = db.Users.FirstOrDefault(u => u.UserId == suaUser.UserId);
+
+            if (user == null)
+            {
+                // Nếu không tìm thấy người dùng, trả về lỗi
+                ModelState.AddModelError("", "User not found.");
+                return View(suaUser);
+            }
+
+            // Kiểm tra xem email mới có trùng với email của người dùng khác không
+            var existingUser = db.Users.FirstOrDefault(u => u.Email == suaUser.Email && u.UserId != user.UserId);
+            if (existingUser != null)
+            {
+                ModelState.AddModelError("", "Email already exists for another user.");
+                return View(suaUser);
+            }
+
+            // Cập nhật thông tin người dùng
+            user.Name = suaUser.Name;
+            user.Email = suaUser.Email;
+            user.Password = suaUser.Password; 
+            user.UserType = suaUser.UserType;
+            // Lưu thay đổi vào cơ sở dữ liệu
+            db.Update(user);
+            db.SaveChanges();
+
+            // Chuyển hướng về danh sách người dùng sau khi sửa thành công
+            return RedirectToAction("DanhMucUser", "admin");
         }
         // appointment
         [Route("DanhMucAppointment")]
@@ -142,8 +195,92 @@ namespace HairHarmonySalon.Areas.Admin.Controllers
             {
                 return RedirectToAction("DanhMucAppointment", "admin");
             }
-            
+
             return View(_Appointment);
+        }
+
+        //Sua Appointment
+        [Route("SuaAppointment")]
+        [HttpGet]
+        public IActionResult SuaAppointment(int id)
+        {
+            var list_stylist = db.Users.Where(u => u.UserType == "Stylist").ToList();
+            var list_customer = db.Users.Where(u => u.UserType == "Customer").ToList();
+            var list_service = db.Services.ToList();
+
+            ViewBag.list_stylist = list_stylist;
+            ViewBag.list_customer = list_customer;
+            ViewBag.list_service = list_service;
+            var appointment = db.Appointments.FirstOrDefault(a => a.AppointmentId == id);
+            if (appointment == null)
+            {
+                // Nếu không tìm thấy appointment, trả về lỗi
+                ModelState.AddModelError("", "Appointment not found.");
+                return RedirectToAction("DanhMucAppointment", "admin");
+            }
+            ViewBag.appointment = appointment;
+
+            //datetime
+            string result_date = "";
+            if (appointment.AppointmentDate != "")
+            {
+                string str_date = appointment.AppointmentDate;
+                string[] parts = str_date.Split(' ');
+
+                // Chuyển đổi sang định dạng 24 giờ
+                DateTime time24Hour = DateTime.ParseExact(parts[0], "h:mmtt", CultureInfo.InvariantCulture);
+                // Định dạng lại thời gian thành HH:mm (24 giờ)
+                result_date = time24Hour.ToString("HH:mm");
+                result_date = parts[1] + "T" + result_date;
+            }
+            ViewBag.date_time = result_date;
+
+            return View();
+        }
+
+        [Route("SuaAppointment")]
+        [HttpPost]
+        public IActionResult SuaAppointment(Appointment appointments)
+        {
+            var appointment = db.Appointments.FirstOrDefault(a => a.AppointmentId == appointments.AppointmentId);
+
+            if (appointment == null)
+            {
+                // Nếu không tìm thấy người dùng, trả về lỗi
+                ModelState.AddModelError("", "User not found.");
+                return RedirectToAction("DanhMucAppointment", "admin");
+            }
+
+            var date_time = appointments.AppointmentDate;
+            string[] parts = date_time.Split('T');
+            TimeSpan timeToCompare = TimeSpan.Parse(parts[1]);
+            TimeSpan noon = TimeSpan.Parse("12:00");
+
+            var string_date = "";
+            if (timeToCompare > noon)
+            {
+                TimeSpan difference = timeToCompare - noon;
+                string_date = difference.ToString(@"hh\:mm") + "PM" + " " + parts[0];
+            }
+            else
+            {
+                string_date = parts[1] + "AM" + " " + parts[0];
+            }
+
+            appointment.CustomerId = appointments.CustomerId;
+            appointment.StylistId = appointments.StylistId;
+            appointment.ServiceId = appointments.ServiceId;
+            appointment.AppointmentDate = string_date;
+            appointment.Status = appointments.Status;
+
+            db.Update(appointment);
+            var result = db.SaveChanges();
+            if (result > 0)
+            {
+                return RedirectToAction("DanhMucAppointment", "admin");
+            }
+
+            return View(appointment);
         }
     }
 }
--- HairmonySalon.WebApplication/Areas/Admin/Controllers/VoucherController.cs
+++ HairmonySalon.WebApplication/Areas/Admin/Controllers/VoucherController.cs
@@ -8,130 +8,130 @@ namespace HairHarmonySalon.Areas.Admin.Controllers
     [Route("admin")]
     [Route("admin/voucher")]
     public class VoucherController : Controller
-        {
-            private readonly HarmonySalonContext _context;
-       
+    {
+        private readonly HarmonySalonContext _context;
+
         public VoucherController(HarmonySalonContext context)
-            {
-                _context = context;
-            }
+        {
+            _context = context;
+        }
         [Route("")]
         [Route("index")]
         [HttpGet]
-            public async Task<IActionResult> Index()
-            {
-                var vouchers = await _context.Vouchers.ToListAsync();
+        public async Task<IActionResult> Index()
+        {
+            var vouchers = await _context.Vouchers.ToListAsync();
 
-                return View(vouchers);
+            return View(vouchers);
+        }
+        [HttpGet]
+
+
+
+        public IActionResult Create()
+        {
+            return View(new Voucher());
+        }
+
+        // POST: Voucher/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("VoucherId,Description,DiscountRate,ValidityPeriod")] Voucher voucher)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(voucher);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index)); // Chuyển hướng tới danh sách voucher
             }
-            [HttpGet]
+            return View(voucher);
+        }
+        private bool VoucherExists(int id)
+        {
+            return _context.Vouchers.Any(e => e.VoucherId == id);
+        }
 
-
-
-            public IActionResult Create()
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
             {
-                return View(new Voucher());
+                return NotFound();
             }
 
-            // POST: Voucher/Create
-            [HttpPost]
-            [ValidateAntiForgeryToken]
-            public async Task<IActionResult> Create([Bind("VoucherId,Description,DiscountRate,ValidityPeriod")] Voucher voucher)
+            var voucher = await _context.Vouchers.FindAsync(id);
+            if (voucher == null)
             {
-                if (ModelState.IsValid)
+                return NotFound();
+            }
+
+            return View(voucher);
+        }
+
+        // POST: Voucher/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("VoucherId,Description,DiscountRate,ValidityPeriod")] Voucher voucher)
+        {
+            if (id != voucher.VoucherId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
                 {
-                    _context.Add(voucher);
+                    _context.Update(voucher);
                     await _context.SaveChangesAsync();
-                    return RedirectToAction(nameof(Index)); // Chuyển hướng tới danh sách voucher
                 }
-                return View(voucher);
-            }
-            private bool VoucherExists(int id)
-            {
-                return _context.Vouchers.Any(e => e.VoucherId == id);
-            }
-
-            public async Task<IActionResult> Edit(int? id)
-            {
-                if (id == null)
+                catch (DbUpdateConcurrencyException)
                 {
-                    return NotFound();
-                }
-
-                var voucher = await _context.Vouchers.FindAsync(id);
-                if (voucher == null)
-                {
-                    return NotFound();
-                }
-
-                return View(voucher);
-            }
-
-            // POST: Voucher/Edit/5
-            [HttpPost]
-            [ValidateAntiForgeryToken]
-            public async Task<IActionResult> Edit(int id, [Bind("VoucherId,Description,DiscountRate,ValidityPeriod")] Voucher voucher)
-            {
-                if (id != voucher.VoucherId)
-                {
-                    return NotFound();
-                }
-
-                if (ModelState.IsValid)
-                {
-                    try
+                    if (!VoucherExists(voucher.VoucherId))
                     {
-                        _context.Update(voucher);
-                        await _context.SaveChangesAsync();
+                        return NotFound();
                     }
-                    catch (DbUpdateConcurrencyException)
+                    else
                     {
-                        if (!VoucherExists(voucher.VoucherId))
-                        {
-                            return NotFound();
-                        }
-                        else
-                        {
-                            throw;
-                        }
+                        throw;
                     }
-                    return RedirectToAction(nameof(Index));
-                }
-                return View(voucher);
-            }
-            // GET: Voucher/Delete/5
-            public async Task<IActionResult> Delete(int? id)
-            {
-                if (id == null)
-                {
-                    return NotFound();
-                }
-
-                var voucher = await _context.Vouchers
-                    .FirstOrDefaultAsync(m => m.VoucherId == id);
-                if (voucher == null)
-                {
-                    return NotFound();
-                }
-
-                return View(voucher);
-            }
-
-            // POST: Voucher/Delete/5
-            [HttpPost, ActionName("Delete")]
-            [ValidateAntiForgeryToken]
-            public async Task<IActionResult> Delete(int id)
-            {
-                var voucher = await _context.Vouchers.FindAsync(id);
-                if (voucher != null)
-                {
-                    _context.Vouchers.Remove(voucher); // Xóa voucher khỏi cơ sở dữ liệu
-                    await _context.SaveChangesAsync(); // Lưu thay đổi
                 }
                 return RedirectToAction(nameof(Index));
             }
-
+            return View(voucher);
         }
+        // GET: Voucher/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var voucher = await _context.Vouchers
+                .FirstOrDefaultAsync(m => m.VoucherId == id);
+            if (voucher == null)
+            {
+                return NotFound();
+            }
+
+            return View(voucher);
+        }
+
+        // POST: Voucher/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var voucher = await _context.Vouchers.FindAsync(id);
+            if (voucher != null)
+            {
+                _context.Vouchers.Remove(voucher); // Xóa voucher khỏi cơ sở dữ liệu
+                await _context.SaveChangesAsync(); // Lưu thay đổi
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
     }
+}
 
 */
